80c0309b46850e2ae3c801c46cd04e16eedb9d41
UserPL135Pc01 <UserPL135Pc01@SLFRPL135PC01.ensino.atec.local>
2016-10-19 12:03:35 +0100

    Aula 0

---
+++ src/Principal.java
@@ -0,0 +1,22 @@
+
+
+public class Principal {
+
+	public Principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá turma\n");
+		
+		String mensagem ="Olá turma, de mensagem";
+		System.out.println(mensagem+"\n");
+		
+		int agua = 40, luz = 20, gas = 20, fibra = 60;
+		int despesas = agua + luz + gas + fibra;
+		System.out.println(despesas+"€\n\n");
+			
+	}
+
+}
